06ddbcac5a97c4d4b300007962b339bbd0b241c8
SahilTrivedi <trivedisahil05@gmail.com>
2016-11-01 09:41:27 +0530

    time

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,13 +12,14 @@ io.on('connection', function(socket) {
 
 	socket.on('message', function(message){
 		console.log('message received : '+ message.text);
-
+		message.timestamp = moment.valueOf();
 		io.emit('message', message);
 	})
 
-	// socket.emit('message', {
-	// 	text:'welcome'
-	// })
+	socket.emit('message', {
+		text:'Welcome to the chat app',
+		timestamp: moment.valueOf()
+	})
 })
 
 http.listen(PORT, function() {
