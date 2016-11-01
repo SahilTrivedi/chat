6e7f91271102f6e2cb12efb814ee768dc5ef0098
SahilTrivedi <trivedisahil05@gmail.com>
2016-11-01 11:20:30 +0530

    room added

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,24 @@
+var name = getQueryVariable('name');
+var room = getQueryVariable('room');
 var socket = io();
+
+$('.room-title').text(room);
 
 socket.on('connect', function(){
 	console.log('connected to socket io server');
+
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 })
 
 socket.on('message', function(message) {
 	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('new : ');
 	console.log(message.text);
-
-	$('.messages').append('<p><strong>'+ momentTimestamp.local().format('h:mm a')  +' : </strong> ' + message.text + '</p>')
+	$('.messages').append('<p><strong>'+ message.name + ' ' + momentTimestamp.local().format('h:mm a')  +' : </strong> '+'</p>')
+	$('.messages').append('<p>'+ message.text + '</p>')
 })
 
 var form = $('#message-form');
@@ -18,6 +27,7 @@ form.on('submit', function(event) {
 	event.preventDefault();
 
 	socket.emit('message', {
+		name:name,
 		text: form.find('input[name=message]').val()
 	})
 
--- server.js
+++ server.js
@@ -5,18 +5,31 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
+var clientInfo = {};
+
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket) {
 	console.log('user connected via socket');
 
+	socket.on('joinRoom', function(req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		})
+	})
+
 	socket.on('message', function(message){
 		console.log('message received : '+ message.text);
 		message.timestamp = moment.valueOf();
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	})
 
 	socket.emit('message', {
+		name:'System',
 		text:'Welcome to the chat app',
 		timestamp: moment.valueOf()
 	})
